37f9171f700afbd6a0909781a9137a74bc35fe14
Arthur Albuquerque <161841244+ArthurArlaxy@users.noreply.github.com>
2026-08-13 13:35:09 -0300

    Fix(Repository): adiciona a opcao null para o tipo de saida do updateProduct

--- Backend/src/Repository/ProductRepository.ts
+++ Backend/src/Repository/ProductRepository.ts
@@ -7,7 +7,7 @@ export interface ProductRepository {
     getProductById(id: string): Promise<Product  | null>
     getProductBySlug(slug: string): Promise<Product  | null>
     deleteProduct(id: string): Promise<Product>
-    updateProduct(id: string, data: ProductUpdateData): Promise<Product>
+    updateProduct(id: string, data: ProductUpdateData): Promise<Product | null>
     addCategoriesToProduct(productId: string, categoryIds: string[]): Promise<void>
     deleteCategoryFromProduct(productId: string, categoryId: string): Promise<void>
     addImagesToProduct(productId: string, data: ImagesProductInput[]): Promise<void>
