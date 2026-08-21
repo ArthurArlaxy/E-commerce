a7501377f9c8fbf739fc03a3fcaaed6cb84459e5
Arthur Albuquerque <161841244+ArthurArlaxy@users.noreply.github.com>
2026-08-21 14:34:11 -0300

    Feat(app/admin): Cria barra de navegacao no layout admin

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -13,20 +13,17 @@ export default function layout({
 
   return (
     <>
-      <header className={styles.adminBar}>
-        <h1 className="title">Administração</h1>
-        <nav>
-            <div className="nav-inner">
-              <Link className={`nav-admin-item ${pathname === "/admin" ? "active" : ''}`} href={"/admin"}>Dashboards</Link>
-              <Link className={`nav-admin-item ${pathname === "/admin/create-product" ? "active" : ''}`} href={"/admin/create-product"}>Create Product</Link>
-              <Link className={`nav-admin-item ${pathname === "/admin/update-product" ? "active" : ''}`} href={"/admin/update-product"}>Update Product</Link>
-            </div>
-        </nav>
-      </header >
+      <h1 className="title">Administração</h1>
+      <nav>
+        <div className="nav-inner">
+          <Link className={`nav-admin-item ${pathname === "/admin" ? "active" : ''}`} href={"/admin"}>Dashboards</Link>
+          <Link className={`nav-admin-item ${pathname === "/admin/create-product" ? "active" : ''}`} href={"/admin/create-product"}>Create Product</Link>
+          <Link className={`nav-admin-item ${pathname === "/admin/update-product" ? "active" : ''}`} href={"/admin/update-product"}>Update Product</Link>
+        </div>
+      </nav>
       <main>
         {children}
       </main>
-      <footer></footer>
     </>
   )
 }
