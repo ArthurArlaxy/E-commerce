d9cfdcc4c2f49c6d5e136e77f1e5aca91e446c6b
Arthur Albuquerque <161841244+ArthurArlaxy@users.noreply.github.com>
2026-08-21 14:37:34 -0300

    Feat(App/register): Coloca a tag main como retorno da página

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -3,21 +3,17 @@ import { registerAction } from "@/actions/auth";
 
 export default function Page() {
     return (
-        <div className="container">
-            <header>
-                <h1 className="title">Tech E-Commerce</h1>
-            </header>
-            <main>
-                <AuthForm
-                    authType="Registrar"
-                    action="criar a"
-                    buttonAction="Criar"
-                    secondAction="Entrar em uma"
-                    buttonSecondAction="Entrar"
-                    hrefPage="/login"
-                    formAction={registerAction}
-                />
-            </main>
-        </div>
+        <main className="container">
+            <h1 className="title">Tech E-Commerce</h1>
+            <AuthForm
+                authType="Registrar"
+                action="criar a"
+                buttonAction="Criar"
+                secondAction="Entrar em uma"
+                buttonSecondAction="Entrar"
+                hrefPage="/login"
+                formAction={registerAction}
+            />
+        </main>
     )
 }
